ad1e9d76c48347a97d9fd787d8a9c7e086d6f87a
Xammed <msk15163@gmail.com>
2020-11-28 23:15:40 -0500

    No Skill Bars

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -39,10 +39,6 @@ export default  class Resume extends Component {
 
             <div className="nine columns main-col">
 
-               <p>
-               A handbag of tricks!
-               </p>
-
    				<div className="bars">
 
    				   <ul className="skills">
@@ -55,7 +51,7 @@ export default  class Resume extends Component {
                       </li>
                     )
                   })*/
-                  <div>
+                  <div> {/*
                   <li>
                       <span className={`bar-expand machine`}></span>
                       <em>Machine Learning and Artificial Intelligence</em>
@@ -75,7 +71,13 @@ export default  class Resume extends Component {
                    <li>
                   <span className={`bar-expand wad`}></span>
                   <em>Web/App Development</em>
-                   </li>
+                  </li> */}
+                  <p>
+                    I am proficient in various machine learning/artificial environments such as Scikit-Learn, TensorFlow, and PyTorch. 
+                    I have experience applying these skills in a data science and research context. I am also profient in Object Oriented
+                    Design Principles, and have a strong proclivity for Algorithm Design. I also dabble in Web/App Development using a variety of tools 
+                    such as Dart/Flutter, React/Node.js, and MongoDB. 
+                  </p>
                    </div>
                   
                 }
@@ -86,65 +88,11 @@ export default  class Resume extends Component {
 
    			</div>
 
-         </div>
-         <div className="row skill">
+         </div> 
+         
 
-            <div className="three columns header-col">
-               <h1><span>Languages</span></h1>
-            </div>
 
-            <div className="nine columns main-col">
 
-               <p>
-               I stand by that programming languages are implied by skills, and don't really
-               need stating because the field always evolves and in order to keep up developers
-               need to constantly learn new languages. However, if you INSIST... Here you go.
-               </p>
-
-   				<div className="bars">
-
-   				   <ul className="skills">
-                {
-                  /*resumeData.skills && resumeData.skills.map((item) => {
-                    return(
-                      <li>
-                      <span className={`bar-expand ${item.skillname.toLowerCase()}`}>
-                      </span><em>{item.skillname}</em>
-                      </li>
-                    )
-                  })*/
-                  <div>
-                  <li>
-                      <span className={`bar-expand python`}></span>
-                      <em>Python</em>
-                  </li>
-                  <li>
-                  <span className={`bar-expand java`}></span>
-                  <em>Java</em>
-                   </li>
-                   <li>
-                  <span className={`bar-expand cpp`}></span>
-                  <em>C/C++</em>
-                   </li>
-                   <li>
-                  <span className={`bar-expand javascript`}></span>
-                  <em>React + JavaScript/Flutter + Dart</em>
-                   </li>
-                   <li>
-                  <span className={`bar-expand misc`}></span>
-                  <em>Prolog/Scala/Ruby/Miscellaneous</em>
-                   </li>
-                   </div>
-                  
-                }
-
-   					</ul>
-
-   				</div>
-
-   			</div>
-
-         </div>
         <div className="row work">
             <div className="three columns header-col">
                <h1><span>Work</span></h1>
@@ -172,6 +120,37 @@ export default  class Resume extends Component {
               }
             </div> 
          </div>
+
+         <div className="row work">
+            <div className="three columns header-col">
+               <h1><span>Workshops</span></h1>
+            </div>
+
+            <div className="nine columns main-col">
+              {
+                resumeData.workshops && resumeData.workshops.map((item) => {
+                  return(
+                    <div className="row item">
+                       <div className="twelve columns">
+                       <h3>{item.CompanyName}</h3>
+                          <p>
+                          {item.Achievements}
+                          </p>
+                          <p>
+                          <a href={item.link}><u>{item.linkname}</u></a>
+                          </p>
+                       </div>
+
+                    </div>
+
+                  )
+                })
+              }
+            </div> 
+         </div>
+
+
+         
 
 
          
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,5 +1,5 @@
 let resumeData = {
-    "imagebaseurl":"https://xammed.github.io/msaroshkhan.com",
+    "imagebaseurl":"https://xammed.github.io/msk.com",
     "name": "M Sarosh Khan",
     "role": "Full Stack Developer",
     "linkedinId":"M Sarosh Khan",
@@ -31,7 +31,7 @@ let resumeData = {
         "specialization":"Master of Science in Data Analytics",
         "MonthOfPassing":"May",
         "YearOfPassing":"2022",
-        "Achievements":"GPA: 3.85/4.00, Dean's List, Honors Student, UPE Member"
+        "Achievements":"GPA: 3.76/4.00, Dean's List, Honors Student, UPE Member"
       },
       {
         "UniversityName":"Rowan University",
@@ -47,28 +47,28 @@ let resumeData = {
         "specialization":"Developer Student Clubs",
         "MonthOfLeaving":"August 2020-",
         "YearOfLeaving":"Present",
-        "Achievements":"Leading, building, and teaching a conglomerate of student developer dedicated to solving community problems with software solutions."
+        "Achievements":"Leading, building, and teaching a conglomerate of student developer dedicated to solving community problems with software solutions. Serving as the DSC Lead for Rowan University, I have always been faced with new challenges. Building software solutions to solve real world problems has highly improved my full-stack development skills."
       },
       {
         "CompanyName":"Image Segmentation Researcher",
         "specialization":"Cooper Hospital via Rowan University",
         "MonthOfLeaving":"December 2020-",
         "YearOfLeaving":"Present",
-        "Achievements":"Working in a team of peers to leverage deep learning algorithms in order to classify the size of brain aneurysms."
+        "Achievements":"Working in a team of peers to leverage deep learning algorithms in order to classify the size of brain aneurysms. This is a new project I have been recruited for, I look forward to learning new skills."
       },
       {
         "CompanyName":"Zero Shot Incremental Learning",
         "specialization":"Rowan University Mathematics Department",
         "MonthOfLeaving":"January 2019-",
         "YearOfLeaving":"December 2020",
-        "Achievements":"Making progress in open set recognition in machine learning using Error Correcting Output Codes. "
+        "Achievements":"Making progress in open set recognition in machine learning using Error Correcting Output Codes. I started working for this research team as a freshman and learned a lot of new skills. Working for this research team I was always faced with new and interesting problems to solve."
       },
       {
         "CompanyName":"Advanced Mathematics Tutor",
         "specialization":"Cherry Hill Tutoring Club",
         "MonthOfLeaving":"September 2018-",
         "YearOfLeaving":"Present",
-        "Achievements":"Making progress in open set recognition in machine learning using Error Correcting Output Codes. "
+        "Achievements":"Tutoring mathematics topics from pre-calculus to differential equations. I got hired by Cherry Hill Tutoring Club shortly graduating high school, this job has definitely improved my soft skills."
       },
       {
         "CompanyName":"Teaching Assistant/Computer Science Tutor",
@@ -76,6 +76,26 @@ let resumeData = {
         "MonthOfLeaving":"January-",
         "YearOfLeaving":"April 2020",
         "Achievements":"Aiding peers in their trajectories through computer science."
+      }
+    ],
+    "workshops":[
+      {
+        "CompanyName":"Python Programming Workshop",
+        "when":"October 2020",
+        "Achievements":"A workshop hosted for the Developer Student Club demonstrating basic/intermediate python programming. ",
+        "linkname": "A Link to the Video",
+        "link":"https://www.youtube.com/watch?v=kbU17GkLMh4&t=4534s"
+      },
+      {
+        "CompanyName":"TensorFlow Developer Series",
+        "when":"December 2020",
+        "Achievements":"A multipart ongoing series of workshops for the Developer Student Club demonstrating deep learning techniques."
+      },
+      
+      {
+        "CompanyName":"Java in a Nutshell",
+        "when":"October 2018",
+        "Achievements":"A presentation on basic Java programming hosted at the Cherry Hill Library for attendees of the EastHacks hackathon."
       }
     ],
     "skillsDescription":"A handbag of tricks...",
